15d38a8ee779ecdeaf93d64188fae57d469e749b
namoo9009 <namoo9009@naver.com>
2022-05-31 05:03:34 +0900

    프로젝트 초기 셋업 & 파이어베이스 추가

---
+++ src/service/firebase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY= AIzaSyDtEOofqZvXSfflbkPG30p582Av_TKxntw,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN= card-maker-dc641.firebaseapp.com,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID= card-maker-dc641,
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
