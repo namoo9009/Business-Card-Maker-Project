6a24272ef73355a1a78683fb57abe1ca3386b86a
namoo9009 <namoo9009@naver.com>
2022-06-28 23:32:11 +0900

    카드 editor, preview 레이아웃 추가

---
+++ src/components/editor/editor.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './editor.module.css';
+
+const Editor = () => {
+  return (
+
+    <section className={styles.editor}>
+        <div className={styles.title}>Card Maker</div>
+    </section>
+  )
+}
+
+export default Editor;
---
+++ src/components/preview/preview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './preview.module.css';
+
+const Preview = () => {
+  return (
+    <section className={styles.preview}>
+        <div className={styles.title}>Card Preview</div>
+    </section>
+  )
+}
+
+export default Preview;
